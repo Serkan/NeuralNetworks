86953332c244eb1372fb64b477d3a2f67af87cea
serkan <serkan.turgut0@gmail.com>
2012-07-28 17:09:48 +0300

    added junit dependency, and main test class

---
+++ src/test/java/org/neuralnetworking/TestRunner.java
@@ -0,0 +1,20 @@
+package org.neuralnetworking;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+/**
+ * Test for several network models.
+ * 
+ * @author serkan
+ * 
+ */
+public class TestRunner {
+
+    @Test
+    public void simplePerceptronTest() {
+        Assert.assertEquals(true, false);
+    }
+
+}
