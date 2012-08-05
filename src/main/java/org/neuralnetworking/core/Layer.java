8d2dc66ee76bb115dbcbbcf5cb05e61afd1b053e
serkan <serkan.turgut0@gmail.com>
2012-08-06 00:12:12 +0300

    constructor changed

--- src/main/java/org/neuralnetworking/core/Layer.java
+++ src/main/java/org/neuralnetworking/core/Layer.java
@@ -3,18 +3,33 @@ package org.neuralnetworking.core;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Container for neurons.
  * 
  * @author serkan
  * 
  */
-public class Layer {
+public class Layer extends AbstractLayer implements Comparable<Layer> {
+
+    Logger logger = LoggerFactory.getLogger(Layer.class);
 
     List<Neuron> neurons;
 
-    public Layer() {
-        neurons = new ArrayList<>();
+    /**
+     * 
+     * Default constructor.
+     */
+    public Layer(int layerSize) {
+        logger.info("Layer id :" + getLayerId());
+        neurons = new ArrayList<Neuron>();
+        Neuron tempNeuron = null;
+        for (int i = 0; i < layerSize; i++) {
+            tempNeuron = new Neuron();
+            neurons.add(tempNeuron);
+        }
     }
 
     public void addNeuron(Neuron neuron) {
@@ -33,6 +48,28 @@ public class Layer {
      */
     public void setNeurons(List<Neuron> neurons) {
         this.neurons = neurons;
+    }
+
+    /**
+     * Size of containing neurons.
+     * 
+     * @return Neuron size
+     */
+    public int getSize() {
+        return neurons.size();
+    }
+
+    @Override
+    public int compareTo(Layer o) {
+        int otherId = o.getLayerId();
+        int layerId = getLayerId();
+        if (otherId < layerId) {
+            return -1;
+        } else if (otherId > layerId) {
+            return 1;
+        } else {
+            return 0;
+        }
     }
 
 }
