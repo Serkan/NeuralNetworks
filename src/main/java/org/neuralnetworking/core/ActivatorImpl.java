e85469d0864a7d5b426e542b13eafe274714d88c
serkan <serkan.turgut0@gmail.com>
2012-08-06 00:15:49 +0300

    no change

--- src/main/java/org/neuralnetworking/core/ActivatorImpl.java
+++ src/main/java/org/neuralnetworking/core/ActivatorImpl.java
@@ -2,7 +2,7 @@ package org.neuralnetworking.core;
 
 public class ActivatorImpl implements ActivatorFunction {
 
-    private final static double THERSHOLD = 0.5;
+    private static double THERSHOLD = 0.5;
 
     @Override
     public boolean activate(double value) {
